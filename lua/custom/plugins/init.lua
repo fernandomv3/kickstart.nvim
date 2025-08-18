55fa4eff28edca8fd714afc95b345ab91bb7269f
fernandomv3 <fernandomv3@gmail.com>
2025-08-18 12:35:04 -0500

    Moved molten config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,6 +10,16 @@ return {
     init = function()
       -- this is an example, not a default. Please see the readme for more configuration options
       vim.g.molten_output_win_max_height = 12
+      vim.g.molten_auto_open_output = false
+      -- this guide will be using image.nvim
+      vim.g.molten_image_provider = 'image.nvim'
+      -- optional, I like wrapping. works for virt text and the output window
+      vim.g.molten_wrap_output = true
+      -- Output as virtual text. Allows outputs to always be shown, works with images, but can
+      -- be buggy with longer images
+      vim.g.molten_virt_text_output = true
+      -- this will make it so the output shows up below the \`\`\` cell delimiter
+      vim.g.molten_virt_lines_off_by_1 = true
     end,
   },
   {
