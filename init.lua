976d382bd26fe6dc0f1d58b81fc34ba24779bb19
fernandomv3 <fernandomv3@gmail.com>
2025-08-18 22:37:52 -0500

    added debugpy, black and ruff

--- init.lua
+++ init.lua
@@ -677,6 +677,10 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        ruff = {},
+        mypy = {},
+        black = {},
+        debugpy = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
