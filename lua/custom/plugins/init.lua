394ac9ce38a5a864970b1605608f91008ff3c1f9
fernandomv3 <fernandomv3@gmail.com>
2025-08-27 10:05:52 -0500

    venv-selector usa main branch y sea agregan bindings para agragar celdas

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -60,7 +60,6 @@ return {
       { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
     },
     lazy = false,
-    branch = 'regexp', -- This is the regexp branch, use this for the new version
     keys = {
       { ',v', '<cmd>VenvSelect<cr>' },
     },
@@ -131,6 +130,87 @@ return {
       vim.keymap.set('n', '<localleader>RA', function()
         runner.run_all(true)
       end, { desc = 'run all cells of all languages', silent = true })
+      local wk = require 'which-key'
+      local is_code_chunk = function()
+        local current, _ = require('otter.keeper').get_current_language_context()
+        if current then
+          return true
+        else
+          return false
+        end
+      end
+
+      --- Insert code chunk of given language
+      --- Splits current chunk if already within a chunk
+      --- @param lang string
+      local insert_code_chunk = function(lang)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'n', true)
+        local keys
+        if is_code_chunk() then
+          keys = [[o```<cr><cr>```{]] .. lang .. [[}<esc>o]]
+        else
+          keys = [[o```{]] .. lang .. [[}<cr>```<esc>O]]
+        end
+        keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+        vim.api.nvim_feedkeys(keys, 'n', false)
+      end
+
+      local insert_r_chunk = function()
+        insert_code_chunk 'r'
+      end
+
+      local insert_py_chunk = function()
+        insert_code_chunk 'python'
+      end
+
+      -- normal mode
+      wk.add({
+        { '<c-LeftMouse>', '<cmd>lua vim.lsp.buf.definition()<CR>', desc = 'go to definition' },
+        { '<c-q>', '<cmd>q<cr>', desc = 'close buffer' },
+        { '<cm-i>', insert_py_chunk, desc = 'python code chunk' },
+        { '<esc>', '<cmd>noh<cr>', desc = 'remove search highlight' },
+        { '<m-I>', insert_py_chunk, desc = 'python code chunk' },
+        { '<m-i>', insert_r_chunk, desc = 'r code chunk' },
+        { '[q', ':silent cprev<cr>', desc = '[q]uickfix prev' },
+        { ']q', ':silent cnext<cr>', desc = '[q]uickfix next' },
+        { 'gN', 'Nzzzv', desc = 'center search' },
+        { 'gf', ':e <cfile><CR>', desc = 'edit file' },
+        { 'gl', '<c-]>', desc = 'open help link' },
+        { 'n', 'nzzzv', desc = 'center search' },
+        { 'z?', ':setlocal spell!<cr>', desc = 'toggle [z]pellcheck' },
+        { 'zl', ':Telescope spell_suggest<cr>', desc = '[l]ist spelling suggestions' },
+      }, { mode = 'n', silent = true })
+
+      -- visual mode
+      wk.add {
+        {
+          mode = { 'v' },
+          { '.', ':norm .<cr>', desc = 'repat last normal mode command' },
+          { '<M-j>', ":m'>+<cr>`<my`>mzgv`yo`z", desc = 'move line down' },
+          { '<M-k>', ":m'<-2<cr>`>my`<mzgv`yo`z", desc = 'move line up' },
+          { '<cr>', send_region, desc = 'run code region' },
+          { 'q', ':norm @q<cr>', desc = 'repat q macro' },
+        },
+      }
+
+      -- visual with <leader>
+      wk.add({
+        { '<leader>d', '"_d', desc = 'delete without overwriting reg', mode = 'v' },
+        { '<leader>p', '"_dP', desc = 'replace without overwriting reg', mode = 'v' },
+      }, { mode = 'v' })
+
+      -- insert mode
+      wk.add({
+        {
+          mode = { 'i' },
+          { '<c-x><c-x>', '<c-x><c-o>', desc = 'omnifunc completion' },
+          { '<cm-i>', insert_py_chunk, desc = 'python code chunk' },
+          { '<m-->', ' <- ', desc = 'assign' },
+          { '<m-I>', insert_py_chunk, desc = 'python code chunk' },
+          { '<m-i>', insert_r_chunk, desc = 'r code chunk' },
+          { '<m-m>', ' |>', desc = 'pipe' },
+        },
+      }, { mode = 'i' })
     end,
   },
 }
