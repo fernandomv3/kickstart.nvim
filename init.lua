06b1f3899c739c99ed67fd85dc7606a06f4bc862
fernandomv3 <fernandomv3@gmail.com>
2026-01-17 23:37:02 -0500

    Use ruff instead of black

--- init.lua
+++ init.lua
@@ -683,7 +683,7 @@ require('lazy').setup({
         ty = {},
         ruff = {},
         -- mypy = {},
-        black = {},
+        -- black = {},
         debugpy = {},
         jsonlint = {},
         xmlformatter = {},
@@ -784,7 +784,7 @@ require('lazy').setup({
         svg = { 'xmlformatter' },
         json = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
